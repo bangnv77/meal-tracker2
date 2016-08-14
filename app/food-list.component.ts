b1447fde7fa0778400b750f4577971e25a084a29
Bang Nguyen <bangnv77@yahoo.com>
2016-08-14 15:56:47 -0700

    add calories.pipe to filter meal items by high or low calorie options

--- app/food-list.component.ts
+++ app/food-list.component.ts
@@ -3,15 +3,23 @@ import { Food } from './food.model';
 import { FoodComponent } from './food.component';
 import { EditFoodDetailsComponent } from './edit-food-details.component';
 import { NewFoodComponent } from './new-food.component';
+import { CaloriesPipe } from './calories.pipe';
 
 @Component({
   selector: 'food-list',
   inputs: ['foodList'],
+  outputs: ['onFoodSelect'],
+  pipes: [CaloriesPipe],
   directives: [FoodComponent, EditFoodDetailsComponent, NewFoodComponent],
   template: `
     <new-food (onSubmitNewFood)='createFood($event)'></new-food>
     <edit-food-details *ngIf="selectedFood" [food]="selectedFood"></edit-food-details>
-    <food-display *ngFor="#currentFood of foodList" [food]="currentFood" (click)="foodClicked(currentFood)" [class.selected]="currentFood === selectedFood"></food-display>
+    <select (change)="onChange($event.target.value)">
+      <option value="all" selected="selected">Show All</option>
+      <option value="high">High Calorie</option>
+      <option value="low">Low Calorie</option>
+    </select>
+    <food-display *ngFor="#currentFood of foodList | calories:filterCalories" [food]="currentFood" (click)="foodClicked(currentFood)" [class.selected]="currentFood === selectedFood"></food-display>
   `
 })
 
@@ -19,6 +27,7 @@ export class FoodListComponent {
   public foodList: Food[];
   public onFoodSelect: EventEmitter<Food>;
   public selectedFood: Food;
+  public filterCalories: string = "all";
 
   constructor() {
     this.onFoodSelect = new EventEmitter();
@@ -32,7 +41,11 @@ export class FoodListComponent {
 
   createFood(params: string[]): void {
     this.foodList.push(
-      new Food(params[0], parseInt(params[1]), params[2], this.foodList.length)  
+      new Food(params[0], parseInt(params[1]), params[2], this.foodList.length)
     );
+  }
+
+  onChange(filterOption) {
+    this.filterCalories = filterOption;
   }
 }
